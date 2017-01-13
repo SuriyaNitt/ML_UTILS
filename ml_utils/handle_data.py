a26a50540d6555b23c9cbfb015dba7597920c9a5
Suriya Narayanan L home <suriyanarayanan987@gmail.com>
2017-01-14 00:04:04 +0530

    Added reading from h5
    Signed-off-by: Suriya Narayanan L home <suriyanarayanan987@gmail.com>

--- ml_utils/handle_data.py
+++ ml_utils/handle_data.py
@@ -1,6 +1,7 @@
 import hickle
 import os
 import numpy as np
+import h5py
 
 def cache_data(data, path):
     if os.path.isdir(os.path.dirname(path)):
@@ -20,3 +21,17 @@ def restore_data(path):
         file = open(path, 'r')
         data = hickle.load(file)
     return data
+
+def print_h5_keys(h5File):
+    f = h5py.File(h5File, 'r')
+    print f.keys()
+
+def read_h5_key_value(h5File, key):
+    f = h5py.File(h5File, 'r')
+    value = f.get(key)
+    return value
+
+def swap_axes(npArray, axis0, axis1):
+    npArray = np.swapaxes(npArray, 1, 2)
+    npArray = np.swapaxes(npArray, 2, 3)
+    return npArray
